0d2e2ea35fe7941eb72b5bb3784911b41eac3a49
thalsi <thalseehthalsi609@gmail.com>
2025-09-14 23:15:58 +0530

    header

--- src/app/index.js
+++ src/app/index.js
@@ -4,3 +4,14 @@ const loader = document.querySelector('[data-loader]');
 window.addEventListener("load", function(){
     setTimeout(() => loader.classList.add('hide'), 2000);
 });
+
+
+const navLinks =document.querySelectorAll('.nav-link');
+
+navLinks.forEach(link=>{
+    link.addEventListener('click',()=>{
+        navLinks.forEach(item=> item.classList.remove('active'));
+
+        link.classList.add('active');
+    });
+});
